af41340a01fc399130044bcb903ff4f1e69e2e6a
Westp233 <Ws15011027055@outlook.com>
2026-04-21 01:07:49 +1000

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        
+        Deque<Character> stack = new ArrayDeque<>();
+        char[] sArr = s.toCharArray();
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('}', '{');
+        map.put(')', '(');
+        map.put(']', '[');
+
+        for (char c : sArr) {
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+                if (top != map.get(c)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
